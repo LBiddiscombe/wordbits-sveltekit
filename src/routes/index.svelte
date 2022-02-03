f4dacc57f6282d1ef21ee1b8fbd436436157d4d6
Lee Biddiscombe <lee.biddiscombe@btinternet.com>
2022-02-03 21:03:47 +0000

    Added DaisyUI

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero min-h-screen bg-base-200">
+  <div class="text-center hero-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">
+            Hello there
+          </h1> 
+      <p class="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p> 
+      <button class="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>

580f6a3f0f57eb36176cc4caa244b8d10688f961
Lee Biddiscombe <lee.biddiscombe@btinternet.com>
2022-02-05 19:45:40 +0000

    added screen to test

--- src/lib/components/ExampleComponent.test.js
+++ src/lib/components/ExampleComponent.test.js
@@ -1,20 +1,20 @@
 // NOTE: jest-dom adds handy assertions to Jest and it is recommended, but not required.
 import '@testing-library/jest-dom';
 
-import { render, fireEvent } from '@testing-library/svelte';
+import { render, screen, fireEvent } from '@testing-library/svelte';
 
 import ExampleComponent from './ExampleComponent.svelte';
 
 it('shows proper heading when rendered', () => {
-	const { getByText } = render(ExampleComponent, { name: 'World' });
+	render(ExampleComponent, { name: 'World' });
 
-	expect(getByText('Hello World!')).toBeInTheDocument();
+	expect(screen.getByText('Hello World!')).toBeInTheDocument();
 });
 
 // Note: This is as an async test as we are using `fireEvent`
 it('changes button text on click', async () => {
-	const { getByText } = render(ExampleComponent, { name: 'World' });
-	const button = getByText('Click Me');
+	render(ExampleComponent, { name: 'World' });
+	const button = screen.getByText('Click Me');
 
 	// Using await when firing events is unique to the svelte testing library because
 	// we have to wait for the next `tick` so that Svelte flushes all pending state changes.
